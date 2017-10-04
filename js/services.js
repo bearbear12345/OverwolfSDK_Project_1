e835918c7cf9dc49e2b56cd85057e1697c83ee50
bearbear12345 <andrew.j.wong@outlook.com>
2017-10-04 22:33:01 +1100

    "min dot js" more like "fin do js" anyway
    housekeeping, minification broke stuff

--- js/services.js
+++ js/services.js
@@ -9,4 +9,20 @@ var services = [
 regex: /.*/,
 request: "https://prod_trackapi:Welcome%40123@digitalapi.auspost.com.au/track/v3/search?q=",
 callback: f=>f.contents.QueryTrackEventsResponse.TrackingResults[0].ReturnMessage.Description},
+{
+name: "WINIT",
+regex: /.*/,
+request: ""
+callback: function(){}
+}
 ]
+/*
+
+Winit
+ID18051539512207CN
+
+??
+89809375563
+
+https://track24.net/
+*/
