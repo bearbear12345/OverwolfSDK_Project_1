6f8a00169d41f5f3472ac7979993431f55be7017
Andrew Wong <andrew12345@live.com>
2017-10-22 15:44:02 +1100

    I screwed up somewhere

--- js/services.js
+++ js/services.js
@@ -4,36 +4,33 @@
  *
  * The following code is licensed under the MIT License
  */
-
-var services = [{ name: "Australia Post",
+var services = [{
+    name: "Australia Post",
     regex: /.*/,
     request: "https://prod_trackapi:Welcome%40123@digitalapi.auspost.com.au/track/v3/search?q=",
-    callback: r =>
-         r.contents.QueryTrackEventsResponse.TrackingResults[0].Consignment.Articles[0].Status;
-     }, {
+    callback: r => r.contents.QueryTrackEventsResponse.TrackingResults[0].Consignment.Articles[0].Status
+  },
+  {
     name: "WINIT",
     regex: /ID:.*/,
     request: "http://track.winit.com.cn/tracking/Index/result/trackingNoString/",
-    callback: r => {
-        new DOMParser().parseFromString(r.contents, "text/html").getElementsByTagName("a")[2].innerHTML.replace(/â€™/, "'").split("&nbsp;", 1)[0];
-    
-}, {
+    callback: r =>
+      new DOMParser().parseFromString(r.contents, "text/html").getElementsByTagName("a")[2].innerHTML.replace(/â€™/, "'").split("&nbsp;", 1)[0]
+  }, {
     // Issue with StarTrack's API is that we can't pass the X-ApiKey header (value: Fabric). Might need to go back to use XMLHttpRequest again. jQuery might be better here??? Or another library
     //https://github.com/axios/axios
     //https://github.com/mjackson/mach
     name: "StarTrack",
     regex: /ST:.*/,
     request: "https://api.startrack.com.au/shipping/v1/consignments?ids=",
-    callback: r => {
-        return 
-    
-}, {
+    callback: r => undefined,
+  }, {
     name: "DHL",
     regex: /DH:.*/,
     request: "http://www.dhl.com.au/shipmentTracking?AWB=",
-    callback: r=> null
-}];
-console.log("END OF SERVICES")
+    callback: r => null
+  }
+];
 //>6 characters
 /*
 function fnDHL(form)
@@ -156,7 +153,6 @@ function fnDHL(form)
   return true;
 }
 */
-
 // Overly complicated.. how about we just send the request and get an error..
 //]
 /*
